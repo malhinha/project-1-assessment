5e58cf4271e0d8e3800fefde2c732617f1c2ef02
John Malhinha <john.malhinha@gmail.com>
2021-06-02 17:34:40 -0700

    project complete (w bonus)

---
+++ js/main.js
@@ -0,0 +1,44 @@
+/* CACHE DOM NODES */
+
+const count = document.getElementById('count');
+const addBtn = document.getElementById('add');
+const subtractBtn = document.getElementById('subtract');
+const integer = document.getElementById('integer');
+
+
+/* GLOBAL VARIABLES */
+let countValue = 0;
+let integerValue = 1;
+
+/* FUNCTIONS */
+
+const countColor = () => {
+  if (countValue < 0) {
+    count.classList.add("red");
+    count.classList.remove("black");
+  }
+  else if (countValue >= 0){
+    count.classList.add("black");
+    count.classList.remove("red");
+  }
+}
+
+/* EVENT LISTENERS */
+
+// add value of integer input to count
+
+addBtn.addEventListener('click', (e) => {
+  let amount = parseInt(integer.value, 10);
+  countValue += amount;
+  count.innerHTML = countValue;
+  countColor();
+});
+
+// subtract value of integer input to count
+
+subtractBtn.addEventListener('click', (e) => {
+  let amount = parseInt(integer.value, 10);
+  countValue -= amount;
+  count.innerHTML = countValue;
+  countColor();
+});
